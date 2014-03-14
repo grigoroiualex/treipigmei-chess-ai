99ee366b3febd23cf420fddd793003ec320bbdb6
grigoroiualex <grigoroiualexandru@gmail.com>
2014-03-14 14:06:53 +0200

    Testing purpose

--- treipigmei-chess-ai/src/chessboard/ChessBoardConnect.java
+++ treipigmei-chess-ai/src/chessboard/ChessBoardConnect.java
@@ -3,5 +3,6 @@ package chessboard;
 public class ChessBoardConnect {
     public static void main(String[] args) {
         System.out.println("TEST");
+        System.out.println("Inca ceva");
     }
 }

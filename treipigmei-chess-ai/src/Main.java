08dcbe76a7bd14084ca1950f8487bca2db6054ca
mei <meirosuandreea@yahoo.com>
2014-03-15 19:42:21 +0200

    Am modificat modul de apelare al protocolului in main; am adaugat noi functii in ChessBoardConnect; la new Move(string) sau movePiece programul crapa.

--- treipigmei-chess-ai/src/Main.java
+++ treipigmei-chess-ai/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         ChessBoardConnect.setProtocolCommands();
 
-        ChessBoardConnect chessProtocol = new ChessBoardConnect();
+        ChessBoardConnect chessProtocol = ChessBoardConnect.getInstance();
         chessProtocol.readInput();
     }
 

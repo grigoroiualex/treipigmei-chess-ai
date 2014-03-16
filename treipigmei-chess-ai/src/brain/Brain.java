e8c07754474e86f6d519ca2425f7ae89ff85e6a0
RemusMaz <remus.mazilu@gmail.com>
2014-03-16 10:40:46 +0200

    Am schimbat variabilele locale de tipul static si am schimbat metoda de returnare a metodei.

--- treipigmei-chess-ai/src/brain/Brain.java
+++ treipigmei-chess-ai/src/brain/Brain.java
@@ -4,35 +4,41 @@ import board.Board;
 import chessboard.ChessBoardConnect;
 
 public class Brain {
-    
-    public static String think() {
-        char [] moveToDo = new char[4];
-        ChessBoardConnect chessBoardConnect = ChessBoardConnect.getInstance();
-        Board board = Board.getInstance();
-        
-        if (chessBoardConnect.getWhiteOnTurn()) {
-            byte i = 6;
-            byte j = 0;
-            while (board.getPiece(new byte[]{i, j}) != null) {
-                i --;
-            }
-            moveToDo[0] = (char)('a' + j);
-            moveToDo[1] = (char)('8' - i);
-            moveToDo[2] = (char)('a' + j);
-            moveToDo[3] = (char)('8' - i - 1);
-        } else {
-            byte i = 1;
-            byte j = 0;
-            while (board.getPiece(new byte[]{i, j}) != null) {
-                i ++;
-            }
-            moveToDo[0] = (char)('a' + j);
-            moveToDo[1] = (char)(i);
-            moveToDo[2] = (char)('a' + j);
-            moveToDo[3] = (char)(i + 1);
-        }
-        
-        return moveToDo.toString();
-    }
+
+	// declarare variabile statice
+	static char[] moveToDo = new char[4];
+
+	static byte whiteRow = 6;
+	static byte blackRow = 1;
+	static byte j = 0;
+
+	public static String think() {
+
+		ChessBoardConnect chessBoardConnect = ChessBoardConnect.getInstance();
+		Board board = Board.getInstance();
+
+		if (chessBoardConnect.getWhiteOnTurn()) {
+
+			if (board.getPiece(new byte[] { whiteRow, j }) != null) {
+				whiteRow--;
+			}
+			moveToDo[0] = (char) ('a' + j);
+			moveToDo[1] = (char) ('8' - whiteRow - 1);
+			moveToDo[2] = (char) ('a' + j);
+			moveToDo[3] = (char) ('8' - whiteRow);
+		} else {
+			if (board.getPiece(new byte[] { blackRow, j }) != null) {
+				blackRow++;
+			}
+			moveToDo[0] = (char) ('a' + j);
+			moveToDo[1] = (char) ('8' - blackRow + 1);
+			moveToDo[2] = (char) ('a' + j);
+			moveToDo[3] = (char) ('8' - blackRow);
+			System.out.println("BLACK <----");
+		}
+
+		// schimbat din moveToDo.toString();
+		return String.valueOf(moveToDo);
+	}
 
 }

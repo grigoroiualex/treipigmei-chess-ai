a1d8dc7f1e2d25145ec0e52674a9e98229498fdc
mei <meirosuandreea@yahoo.com>
2014-05-06 23:27:37 +0300

    Implemented alfa-beta algorithm

--- treipigmei-chess-ai/src/brain/Brain.java
+++ treipigmei-chess-ai/src/brain/Brain.java
@@ -19,6 +19,7 @@ public class Brain {
     static int[] from = new int[2];
     static int[] to = new int[2];
     public static Move bestMove;
+    public static Move badMove;
     
     /**
      * Implementation of the negamax algorithm
@@ -62,5 +63,49 @@ public class Brain {
         }
         return bestScore;
     }
-
+    
+    public static int alfaBeta(Flags.Colour playerColor, Clone chessBoard, int depth, int alfa, int beta) {
+        
+        if(depth == 0) {
+            Evaluation evaluatedBoard = new Evaluation(chessBoard);
+            return evaluatedBoard.eval();
+        }
+        ArrayList<Move> moves = chessBoard.getAllMoves();
+        
+        if (moves.isEmpty()) {
+            if (chessBoard.getWhites().size() == 1
+                    && chessBoard.getBlacks().size() == 1) {
+                return 0;
+            } else {
+                return 10000;
+            }
+        }
+        
+        int bestScore = Integer.MIN_VALUE;
+        
+        if (moves != null) {
+            for(Move m : moves) {
+                Clone clonedBoard = chessBoard.newClone();
+                clonedBoard = clonedBoard.getCloneWithMove(m);
+                int score = -alfaBeta(playerColor, clonedBoard, depth - 1, -beta, -alfa);
+                if(bestScore >= alfa) {
+                    alfa = bestScore;
+                }
+                if(score >= beta) {
+                    return beta;
+                }
+                if(score > alfa) {
+                    alfa = score;
+                    bestScore = score;
+                    if(depth == Flags.NEGAMAX_DEPTH) {
+                        bestMove = m;
+                    }                  
+                }
+            }
+        }
+        if (bestMove == null) {
+                bestMove = moves.get(0);
+        }
+        return bestScore;
+    }
 }
